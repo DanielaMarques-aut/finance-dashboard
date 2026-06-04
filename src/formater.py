5cedb28988b78494e2d3940dd9406c1f28b79c59
Daniela Marques <daniqmarques@hotmail.com>
2026-06-04 18:36:29 +0100

    added excel chart to formatter

--- src/formater.py
+++ src/formater.py
@@ -1,5 +1,6 @@
 from openpyxl.styles import Font, Alignment, PatternFill, Border, Side, numbers
 from openpyxl.utils import get_column_letter
+from openpyxl.chart import BarChart, Reference, PieChart
 
 # Color palette
 COLORS = {
@@ -50,4 +51,30 @@ def add_alternating_row_colors(worksheet, start_row, end_row, col_num):
                     )
 def freeze_header(worksheet):
     """Freezes the header row."""
-    worksheet.freeze_panes = "A2"       
+    worksheet.freeze_panes = "A2"   
+
+def add_spending_by_category_chart(category_sheet, workbook):
+    """Adds a bar chart for spending by category."""
+    worksheet= workbook[category_sheet]
+    max_row = worksheet.max_row
+    
+      
+    # Create the bar chart
+    chart = BarChart()
+    chart.type = "col"
+    chart.style = 10
+    chart.width = 20
+    chart.height = 12
+    chart.title = "Spending by Category"
+    chart.x_axis.title = "Category"
+    chart.y_axis.title = "Amount (€)"
+    
+    data = Reference(worksheet, min_col=2, min_row=1, max_row=max_row)
+    categories_ref = Reference(worksheet, min_col=1, min_row=2, max_row=max_row)
+    
+    chart.add_data(data, titles_from_data=True)
+    chart.set_categories(categories_ref)
+    
+    # Position the chart
+    worksheet.add_chart(chart, "D2")
+    return workbook
